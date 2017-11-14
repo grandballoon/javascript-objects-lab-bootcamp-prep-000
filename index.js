ad93b432f1dc3adc238900607c9945aee04a70cc
Luke Conley <luke.sovern.conley@gmail.com>
2017-11-14 02:04:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 var newObj = Object.assign({}, object, {[key]:value})
 return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  var destructObj = Object.assign(object, {[key]: value})
+  return destructObj;
+}
