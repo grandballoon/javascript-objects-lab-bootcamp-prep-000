fa9e5f45d929a2d9fe600814e11f28cce4db5034
Luke Conley <luke.sovern.conley@gmail.com>
2017-11-14 01:15:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 object.key = value;
 return object
 
-var newObject = Object.assign({}, object)
+var newObject = Object.assign({}, object);
 return newObject
 }
